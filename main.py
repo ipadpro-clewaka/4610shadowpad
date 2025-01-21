9d6d9271c2383ba2a926d47d8ef5f85b40543391
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2025-01-21 15:09:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ token = "e4f5c13f-4f31-4ae1-ac5c-b3f1df232073" # hcaptchaのサイトキー
 max_api_wait_time = 5
 max_time = 10
 # "https://invidious.adminforge.de/",
-apis = ["https://invidious.f5.si/","https://lekker.gay/",'https://inv.nadeko.net/','https://inv1.nadeko.net/', 'https://inv2.nadeko.net/','https://inv3.nadeko.net/','https://inv4.nadeko.net/','https://inv5.nadeko.net/','https://inv.zzls.xyz/', 'https://invidious.nerdvpn.de/', 'https://iv.melmac.space/','https://youtube.lurkmore.com/',"https://iv.duti.dev/"]
+apis = ["https://invidious.ducks.party/","https://invidious.f5.si/","https://lekker.gay/",'https://inv.nadeko.net/','https://inv1.nadeko.net/', 'https://inv2.nadeko.net/','https://inv3.nadeko.net/','https://inv4.nadeko.net/','https://inv5.nadeko.net/','https://inv.zzls.xyz/', 'https://invidious.nerdvpn.de/', 'https://iv.melmac.space/','https://youtube.lurkmore.com/',"https://iv.duti.dev/"]
 version = "1.0"
 adminannounce = requests.get(r'https://ztttas1.github.io/yuki00000000000000000000000000000/AN.txt').text.rstrip()
 
